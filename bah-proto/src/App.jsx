dfa4e804a5ac311fc95b3503d66812ddfafd003a
ansh-arora12 <ansharora1223@gmail.com>
2025-07-07 16:25:22 +0530

    Update App.jsx

--- bah-proto/src/App.jsx
+++ bah-proto/src/App.jsx
@@ -2,16 +2,26 @@ import "./App.css";
 import MapComponent from "../components/mapComponent";
 function App() {
   return (
-    <>  <div id="a">
-  <img src="Screenshot 2025-07-07 133609.png" alt="logo" />
-  <div id="other">
-    <h4>SVG</h4>
-    <h4>ChatBot</h4>
-    <h4>Backtracking</h4>
-    <button id="pehla">SignUp</button>
-    <button id="dusra">LogIn</button>
+    <>   <div id="a">
+        <img id="svg" src="Screenshot 2025-07-07 133609.png">
+        <div id="other">
+        <h4>ChatBot</h4>
+        <h4>Backtracking</h4>
+        <img id="o" src="icons8-language-50.png" alt="">
+        <button id="pehla">SignUp</button>
+        <button id="dusra">LogIn</button>
+      </div>
+    </div>
+   <div class="navbar-placeholder">
+    
   </div>
-</div>
+
+  <!-- Compact Sidebar -->
+  <div class="sidebar">
+    <h2 style="color: black;">PM</h2>
+    <button class="btn">PM 2.5</button>
+    <button class="btn">PM 10</button>
+  </div>
       
 
       <div>
