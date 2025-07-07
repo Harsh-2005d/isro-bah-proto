2a432f9d7dad81a7eb58094060b307f98288fb80
Jon Snow <dahiyaharsh2005@gmail.com>
2025-07-07 14:18:40 +0530

    a

--- bah-proto/components/mapComponent.jsx
+++ bah-proto/components/mapComponent.jsx
@@ -1,28 +1,41 @@
-
-import { useEffect, useRef } from 'react';
-import L from 'leaflet';
+import { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet.heat"; // required for heatLayer
+import { heatLayer } from "leaflet-heat-es";
+import { heatData } from "./data";
 
 const MapComponent = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    
     if (mapRef.current) return;
 
-    const map = L.map('map').setView([22.5937, 78.9629], 5); 
+    const map = L.map("map").setView([22.59, 78.96], 5);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors',
+    // OSM base map
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+      maxZoom: 19,
+    }).addTo(map);
+
+    // Heatmap data
+    const points = heatData;
+
+    heatLayer(points, {
+      radius: 50,
+      blur: 25,
+      gradient: {
+        0.2: "#00f",
+        0.4: "#0f0",
+        0.6: "#ff0",
+        1.0: "#f00",
+      },
     }).addTo(map);
 
     mapRef.current = map;
-
-   
   }, []);
 
-  return (
-    <div id="map" style={{ height: '100vh', width: '100%' }}></div>
-  );
+  return <div id="map" style={{ height: "100vh", width: "100%" }} />;
 };
 
 export default MapComponent;
--- bah-proto/src/App.jsx
+++ bah-proto/src/App.jsx
@@ -1,13 +1,27 @@
-
-import './App.css'
-import MapComponent from '../components/mapComponent'
+import "./App.css";
+import MapComponent from "../components/mapComponent";
 function App() {
-  return(
-    <div>
-      <MapComponent/>
-    </div>
-  )
-  
+  return (
+    <>  <div id="a">
+  <img src="Screenshot 2025-07-07 133609.png" alt="logo" />
+  <div id="other">
+    <h4>SVG</h4>
+    <h4>ChatBot</h4>
+    <h4>Backtracking</h4>
+    <button id="pehla">SignUp</button>
+    <button id="dusra">LogIn</button>
+  </div>
+</div>
+      
+
+      <div>
+        <h1>HIII</h1>
+      </div>
+      <div>
+        <MapComponent />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
